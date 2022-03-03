4836104918f545defe147d081b64ec335c09d3d0
emamaria <emascript@gmail.com>
2022-03-03 21:55:11 +0100

    chore: add iteracion2

---
+++ iteracion2.js
@@ -0,0 +1,59 @@
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+           
+        }
+    },
+]
+
+function averageVolume(){
+
+let count = 0;
+
+let sum = 0
+
+for(let user of users){
+    for(let type in user.favoritesSounds){
+         sum += user.favoritesSounds[type].volume;
+         count++;
+    }
+  
+}
+
+
+ 
+ return "The average volume  of users favourite sounds is " + sum/count;
+
+}
+
+
+console.log(averageVolume());
+
+
+
+
+
